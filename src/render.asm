a0c175134c2ee33666d49741fab6d4b2a21757f3
Julia Sakharova <y.sakharova@ukma.edu.com>
2026-03-22 01:19:10 +0200

    feat: implement num_to_str procedure

--- src/render.asm
+++ src/render.asm
@@ -81,13 +81,46 @@ animate_tile PROC ; КТ-5-6
     animate_tile ENDP
 
 num_to_str PROC ; КТ-3
-; відображення числа в рядок
+; відображення числа в рядок та знаходження довжини
     push bp
     mov bp, sp
+    push ax
+    push bx
+    push dx
+    push di
 
-    ; ділити на 10, записуючи залишок в стек
-    ; витягувати, додаючи 30h ('0')
+    ; повернути рядок в buffer, довжину в cx
+    ; [bp+4] - значення плитки
+    mov ax, [bp+4]
+    xor cx, cx
+    mov bx, 10
 
+    mov di, offset buffer
+
+    convert:
+    xor dx, dx
+    div bx          ; ax - частка, dx - остача
+    push dx
+    inc cx
+    cmp ax, 0
+    jne convert
+
+    mov bx, cx      ; зберігти довжину
+
+    write:
+    pop dx
+    add dl, '0'
+    mov [di], dl
+    inc di
+    loop write
+    mov byte ptr [di], '$'
+
+    mov cx, bx      ; повертаємо довжину
+
+    pop di
+    pop dx
+    pop bx
+    pop ax
     pop bp
     ret
     num_to_str ENDP
