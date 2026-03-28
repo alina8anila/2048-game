3639dc2cbaf975f930c4e595c15bc5e6408667dd
Alina8anila <a.parashchii@ukma.edu.ua>
2026-03-28 17:13:52 +0200

    feat: implement slide_down PROC

--- src/testlog.asm
+++ src/testlog.asm
@@ -3,10 +3,10 @@
 locals @@
 
 .DATA
-    board   DB 1, 3, 0, 2
+    board   DB 1, 3, 2, 2
             DB 0, 3, 0, 1
-            DB 0, 1, 0, 1
-            DB 1, 3, 2, 2
+            DB 3, 1, 0, 1
+            DB 1, 3, 2, 1
     
     row  db 1, 0, 1, 2
 
@@ -15,7 +15,7 @@ start:
     mov ax, @data
     mov ds, ax
 
-    call slide_up
+    call slide_down
 
     push offset board
     call print
