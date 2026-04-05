02b64b56b242f45d1f47a5c68a18eeddfa5de4f7
Alina8anila <a.parashchii@ukma.edu.ua>
2026-04-05 10:06:09 +0300

    test: score

--- src/testlog.asm
+++ src/testlog.asm
@@ -3,53 +3,36 @@
 locals @@
 
 .DATA
-    board   DB 1, 1, 1, 1 
+    board   DB 2, 2, 1, 1 
+            DB 1, 3, 3, 1
             DB 1, 1, 1, 1
-            DB 1, 1, 6, 1
-            DB 1, 10, 12, 1
+            DB 1, 1, 2, 2
 
-    board2 DB 16 DUP(0)
-    
+    saveboard DB 16 DUP(0)
+    prevboard DB 16 DUP(0)
+    game_phase  DB 0         ;0-game is going, 1-win, 2-lose
     row  db 1, 0, 1, 2
+    curr_score  DW 0
 
 .CODE
 start:
     mov ax, @data
     mov ds, ax
 
-    push offset board  ;to
-    push offset board2 ;form
-    call compare_boards
-    add sp, 4
-    mov ah, 02h
-    mov dl, al
-    add dl, '0'
-    int 21h
-
-    push offset board  ;to
-    push offset board2 ;form
-    call copy_boards
-    add sp, 4
-
+    push offset board
+    call slide_left
+    add sp, 2
     push offset board
     call print
     add sp, 2
 
-    push offset board  ;to
-    push offset board2 ;form
-    call compare_boards
-    add sp, 4
+    mov dl, 13
     mov ah, 02h
-    mov dl, al
-    add dl, '0'
     int 21h
 
-    ;call slide_left
-    ;push offset board
-    ;call print
-    ;add sp, 2
-    ;mov dl, 10      ; Line Feed
-    ;int 21h
+    push curr_score
+    call print_score
+    add sp, 2
 
     ;call slide_right
     ;push offset board
@@ -117,6 +100,38 @@ start:
         pop bp
         ret
     print ENDP
+
+    print_score PROC
+        push ax
+        push bx
+        push cx
+        push dx
+
+        mov ax, [curr_score] 
+        mov bx, 10           
+        xor cx, cx           
+
+    @@divide_loop:
+        xor dx, dx           
+        div bx               
+        push dx             
+        inc cx               
+        test ax, ax          
+        jnz @@divide_loop   
+
+    @@display_loop:
+        pop dx              
+        add dl, '0'         
+        mov ah, 02h         
+        int 21h
+        loop @@display_loop  
+
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
+    print_score ENDP
 
     INCLUDE logic.asm
 end start
